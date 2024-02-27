e89a5c0344744d088c702ab0f2b1774797f7ed03
Pouya Tanouri <ptanouri@Pouyas-MacBook-Air.local>
2024-02-27 00:41:57 -0800

    batch training updated

--- Batch_Training.py
+++ Batch_Training.py
@@ -18,12 +18,14 @@ import math
 # training loop
 
 class WineDataset:
-    def __init__(self):
+    def __init__(self, transform = None):
         # data loading
-        xy = np.loadtxt('wine.csv', delimiter=",", dtype=np.float32, skiprows=1)
-        self.x = torch.from_numpy(xy[:, 1:])
-        self.y = torch.from_numpy(xy[:, [0]]) # n_samples. 1
+        xy = np.loadtxt('../data/wine.csv', delimiter=",", dtype=np.float32, skiprows=1)
+        self.x = xy[:, 1:]
+        self.y = xy[:, [0]] # n_samples. 1
         self.n_samples = xy.shape[0]
+        
+        self.transform = transform
         # data = datasets.load_wine()
         # self.x = torch.from_numpy(data.data.astype(np.float32))
         # self.y = torch.from_numpy(data.target.astype(np.float32))
@@ -32,31 +34,63 @@ class WineDataset:
 
     def __getitem__(self, index):
         #dataset[0]
-        return self.x[index], self.y[index]
+        sample = self.x[index], self.y[index]
+        
+        if self.transform:
+            sample = self.transform(sample)
+        return sample
 
     def __len__(self):
         # len(dataset)
         return self.n_samples
     
-dataset = WineDataset()
-dataloader = DataLoader(dataset=dataset, batch_size=4, shuffle=True)
+class ToTensor:
+    def __call__(self, sample):
+        inputs, targets = sample
+        return torch.from_numpy(inputs), torch.from_numpy(targets)
+    
+class MulTransform:
+    def __init__(self, factor):
+        self.factor = factor
+        
+    def __call__(self, sample):
+        inputs, targets = sample
+        inputs *= self.factor
+        return inputs, targets
+    
+    
+dataset = WineDataset(transform=ToTensor())
+first_data = dataset[0]
+features, labels = first_data
+print(features)
+print(type(features), type(labels))
 
-#training loop
-num_epochs = 2
-total_samples = len(dataset)
-n_iterations = math.ceil(total_samples/4)
-print(total_samples, n_iterations)
 
-for epoch in range(num_epochs):
-    for i, (inputs, labels) in enumerate(dataloader):
-        # forward, backward, update
-        if (i+1) % 5 == 0:
-            print(f'epoch {epoch+1}/{num_epochs}, step {i+1}/{n_iterations}, inputs {inputs.shape}')
+composed = transforms.Compose([ToTensor(), MulTransform(4)])
+dataset = WineDataset(transform=composed)
+first_data = dataset[0]
+
+features, labels = first_data
+print(type(features), type(labels))
+print(features)
+# dataloader = DataLoader(dataset=dataset, batch_size=4, shuffle=True)
+
+# #training loop
+# num_epochs = 2
+# total_samples = len(dataset)
+# n_iterations = math.ceil(total_samples/4)
+# print(total_samples, n_iterations)
+
+# for epoch in range(num_epochs):
+#     for i, (inputs, labels) in enumerate(dataloader):
+#         # forward, backward, update
+#         if (i+1) % 5 == 0:
+#             print(f'epoch {epoch+1}/{num_epochs}, step {i+1}/{n_iterations}, inputs {inputs.shape}')
 
 
-# dataiter = iter(dataloader)
-# if dataiter:
-#     data = next(dataiter)
+# # dataiter = iter(dataloader)
+# # if dataiter:
+# #     data = next(dataiter)
 #     features, labels = data
 #     print(features, labels)
 # else:
